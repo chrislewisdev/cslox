7d831dcdf8168b69f93037b1fb273a2a7be85391
Chris Lewis-Hou <Chris.Lewis-Hou@ratemyagent.com>
2023-10-05 17:20:37 +1100

    Basic main method

---
+++ Lox.cs
@@ -0,0 +1,20 @@
+﻿namespace cslox;
+class Lox
+{
+    static void Main(string[] args)
+    {
+        if (args.Length > 1)
+        {
+            Console.WriteLine("Usage: cslox [script]");
+            Environment.Exit(64);
+        }
+        else if (args.Length == 1)
+        {
+            //RunFile(args[0]);
+        }
+        else
+        {
+            //RunPrompt();
+        }
+    }
+}
